38e73a69a9f272a9fe4b4183106ee46a5358029c
whstruts <whstruts@163.com>
2019-11-26 10:30:05 +0800

    Table 点击事件 并且传值  handleSearchGoods

--- src/components/Selector/ItemType/ItemType.vue
+++ src/components/Selector/ItemType/ItemType.vue
@@ -2,7 +2,7 @@
   <a-select
     showSearch
     labelInValue
-    :value="value"   
+    :value="value"
     placeholder="请选择"
     style="width: 100%"
     :filterOption="filterOption"
@@ -11,64 +11,79 @@
     :notFoundContent="fetching ? undefined : null"
   >
     <a-spin v-if="fetching" slot="notFoundContent" size="small"/>
-    <a-select-option v-for="d in data" :key="d.value">{{d.text}}</a-select-option>
+    <a-select-option v-for="d in data" :key="d.value">{{ d.text }}</a-select-option>
   </a-select>
 </template>
 <script>
-import jsonp from 'fetch-jsonp';
-import querystring from 'querystring';
-import debounce from 'lodash/debounce';
-import api from '@/api';
+import debounce from 'lodash/debounce'
+import api from '@/api'
 
 export default {
-  name: 'ItemType', 
-  data() {
-    this.lastFetchId = 0;
-    this.fetchItemType = debounce(this.fetchItemType, 800);
+  name: 'ItemType',
+  data () {
+    this.lastFetchId = 0
+    this.fetchItemType = debounce(this.fetchItemType, 800)
     return {
       data: [],
-      value:'',      
+      value:'',
       fetching: false,
-      itemTypeId: "0",
+      itemTypeId: '0',
       itemType:{}
     }
   },
 
   props:{
   },
-
+  mounted (){
+    this.lastFetchId += 1
+    const fetchId = this.lastFetchId
+    this.data = []
+    this.fetching = true
+    fetch(api.FindAllItemType)
+      .then(response => response.json())
+      .then((body) => {
+        if (fetchId !== this.lastFetchId) { // for fetch callback order
+          return
+        }
+        const data = body.data.map(t => ({
+          text: t.name,
+          value: t.id
+        }))
+        this.data = data
+        this.fetching = false
+      })
+  },
   methods: {
     fetchItemType (value) {
-      console.log('fetching itemType', value);
-      this.lastFetchId += 1;
-      const fetchId = this.lastFetchId;
+      console.log('fetching itemType', value)
+      this.lastFetchId += 1
+      const fetchId = this.lastFetchId
       this.data = []
       this.fetching = true
       fetch(api.FindAllItemType)
         .then(response => response.json())
         .then((body) => {
           if (fetchId !== this.lastFetchId) { // for fetch callback order
-            return;
+            return
           }
           const data = body.data.map(t => ({
-            //text: `${t.name.first} ${user.name.last}`,
             text: t.name,
             value: t.id
-          }));
+          }))
           this.data = data
           this.fetching = false
-        });
+        })
     },
     handleChange (value) {
       Object.assign(this, {
         value,
         data: [],
-        fetching: false,
+        fetching: false
       })
-      this.itemTypeId=this.value.key
-      this.itemType={id:this.value.key}
+      this.itemTypeId = this.value.key
+      this.itemType = { id:this.value.key }
     },
-    filterOption(input, option) {
+    filterOption (input, option) {
       return option.componentOptions.children[0].text.toLowerCase().indexOf(input.toLowerCase()) >= 0
     }
   }
